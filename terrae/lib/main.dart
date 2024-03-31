ad765576e571bf45bdb37163bec114b59730f92e
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-03-31 23:48:05 +0100

    test

--- terrae/lib/main.dart
+++ terrae/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:terrae/quiz/quiz.dart';
 
-void main() {
+const supabaseUrl = 'https://exavhaoplucfxnfiavym.supabase.co';
+const supabaseKey = "";
+
+Future<void> main() async {
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
   runApp(const App());
-  
 }
 
 class App extends StatelessWidget {
