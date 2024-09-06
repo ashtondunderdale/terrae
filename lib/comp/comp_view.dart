301fdd7d8da436480066af0808d2a47d539e4f19
rxgq <ashtondunderdale@gmail.com>
2024-09-06 21:56:44 +0100

    memory

--- lib/comp/comp_view.dart
+++ lib/comp/comp_view.dart
@@ -9,14 +9,74 @@ class CompView extends StatefulWidget {
 }
 
 class _CompViewState extends State<CompView> {
-  final vm = VM();
+  final vm = VM(); // Assuming this initializes the VM and its memory
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 300,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 240, 240, 240),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 600,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 240, 240, 240),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: _buildMemory(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
+  Widget _buildMemory() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 8,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: vm.memory.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.black26),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Center(
+              child: Text(
+                '0x${index.toRadixString(16).padLeft(2, '0').toUpperCase()}: ${vm.memory[index]}',
+                style: const TextStyle(
+                  fontSize: 12, 
+                  color: Colors.black
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
